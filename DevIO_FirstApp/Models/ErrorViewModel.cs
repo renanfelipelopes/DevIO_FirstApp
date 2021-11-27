f62a6501251dde06ce606f3de0a32a3e49e193b6
Renan Lopes <renanlopes.ads@gmail.com>
2021-11-26 23:05:58 -0300

    init project

---
+++ DevIO_FirstApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DevIO_FirstApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
